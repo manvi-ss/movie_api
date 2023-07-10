ad4ba1c52fb6f2c321aaf1b0fb1803d61335c3fb
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-07-10 18:24:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ const Users = Models.User;
 
 
 //mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser:true, useUnifiedTopology:true});
-mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(() => console.log('Connected to database'))
+.catch((error) => console.log('Error message:', error.message ));
 
 
 
