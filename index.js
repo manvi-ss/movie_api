1f8724cb4d2b25689effdcb027df88d4572bd569
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-07-10 17:11:57 +0200

    mongoose.connect updated with environmental variables

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ const Users = Models.User;
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser:true, useUnifiedTopology:true});
+//mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser:true, useUnifiedTopology:true});
 
 
 
